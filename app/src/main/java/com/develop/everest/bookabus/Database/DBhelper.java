84f25ef9dd699dc8dcf784deb16f0aeebebfc100
Shubham <shubham.dalmia@live.com>
2019-02-20 23:39:23 +0530

    Database Created. Insert Func Added. #3

---
+++ app/src/main/java/com/develop/everest/bookabus/Database/DBhelper.java
@@ -0,0 +1,53 @@
+package com.develop.everest.bookabus.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.net.Inet4Address;
+
+public class DBhelper extends SQLiteOpenHelper {
+    public static final String DB_Name = "BUS.db";
+    public static final String TABLE_Name = "Search_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "Dest_To";
+    public static final String COL_3 = "Dest_From";
+    public static final String COL_4 = "NAME";
+    public static final String COL_5 = "TIME";
+    public static final String COL_6 = "PRICE";
+
+    //Whenever this constructor is called our DB will be created
+    public DBhelper(Context context) {
+        super(context, DB_Name, null, 1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_Name + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,Dest_To TEXT,Dest_From TEXT, NAME TEXT, TIME TEXT, PRICE INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("Drop Table IF  EXISTS " + TABLE_Name);
+        onCreate(db);
+    }
+
+    public boolean insertData(String Dest, String Src, String name, String Time, String price) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, Dest);
+        contentValues.put(COL_3, Src);
+        contentValues.put(COL_4, name);
+        contentValues.put(COL_5, Time);
+        contentValues.put(COL_6, price);
+        long res = db.insert(TABLE_Name, null, contentValues);
+        if (res == -1)
+            return false;
+        else
+            return true;
+
+
+    }
+}
---
+++ app/src/main/java/com/develop/everest/bookabus/Result.java
@@ -0,0 +1,47 @@
+package com.develop.everest.bookabus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.develop.everest.bookabus.Database.DBhelper;
+
+public class Result extends AppCompatActivity {
+    DBhelper my_db;
+    EditText Dest,Src,Name,Time,Price;
+    Button btn_Add;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        my_db = new DBhelper(this);
+        Dest = (EditText)findViewById(R.id.Destination);
+        Src = (EditText)findViewById(R.id.Source);
+        Name = (EditText)findViewById(R.id.Name);
+        Time = (EditText)findViewById(R.id.Time);
+        Price = (EditText)findViewById(R.id.Price);
+        btn_Add = (Button)findViewById(R.id.Add);
+        AddData();
+    }
+
+    public void AddData(){
+        btn_Add.setOnClickListener( new View.OnClickListener(){
+            public void onClick(View v){
+                boolean isInserted = my_db.insertData(Dest.getText().toString(),
+                        Src.getText().toString(),
+                        Name.getText().toString(),
+                        Time.getText().toString(),
+                        Price.getText().toString());
+                if( isInserted == true)
+                    Toast.makeText(Result.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(Result.this,"Error",Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+}
